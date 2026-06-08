8ba6f8ed7868d510d895c266f83d25799e3b36d8
VelocityWrld <victorobafemi606@gmail.com>
2026-06-08 23:14:22 +0100

    Update server.py

--- server.py
+++ server.py
@@ -32,6 +32,6 @@ def get_ip_info(ip_address: str) -> str:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app = Starlette(routes=[
-       Mount("/", app=mcp.sse_app()),
-])
+        Mount("/", app=mcp.sse_app()),
+    ])
     uvicorn.run(app, host="0.0.0.0", port=port)
