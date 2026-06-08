e8a89b8a5f434ce4c65172d8046ab720fbc5e175
VelocityWrld <victorobafemi606@gmail.com>
2026-06-08 22:55:46 +0100

    Update server.py

--- server.py
+++ server.py
@@ -29,5 +29,5 @@ def get_ip_info(ip_address: str) -> str:
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app = mcp.http_app(transport="sse")
+    app = mcp.sse_app(transport="sse")
     uvicorn.run(app, host="0.0.0.0", port=port)
